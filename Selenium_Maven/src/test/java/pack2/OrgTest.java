0054234c64c1fd8886347f083213e1f55b5b3e7b
Samatham-sam <samtham.kalyani@gmail.com>
2024-09-20 10:49:40 +0530

    first push

---
+++ Selenium_Maven/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,17 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("execute createOrg");
+	}
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("execute modifyOrg");
+	}
+}
